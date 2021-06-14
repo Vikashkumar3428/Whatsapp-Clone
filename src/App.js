17692670b4f4fa255de2f78bd82f2f0c3d3dfc55
Vikash Kumar <42893025+Vikashkumar3428@users.noreply.github.com>
2021-06-14 15:35:21 +0530

    chat body implemented, Included chat message

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ function App() {
   return (
     // BEM  naming convention
     <div className="App">
-        <h1>Whatsapp Clone</h1>
         <div className="app__body">
           <Sidebar/>
           <Chat />
--- src/Chat.jsx
+++ src/Chat.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
 import './Chat.css';
-import { Avatar } from '@material-ui/core';
+import { Avatar,IconButton } from '@material-ui/core';
+import { AttachFile, InsertEmoticon } from '@material-ui/icons';
+import MoreVertIcon from  '@material-ui/icons/MoreVert';
+import SearchOutLined from  '@material-ui/icons/SearchOutlined';
 
 function Chat() {
     return (
         <div className="chat">
             <div className="chat__header">
                 <Avatar />
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen....</p>
+                </div>
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutLined />
+                    </IconButton>
+                    
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                    
+                </div>
             </div>
             <div className="chat__body">
-
+                <p className="chat__message">Hello</p>
+                <p className="chat__message">Hii</p>
+                <p className="chat__message">Busy Person</p>
             </div>
             <div className="chat__footer">
-
+                    <IconButton>
+                        <InsertEmoticon/>
+                    </IconButton>
+                    <IconButton>
+                        <AttachFile/>
+                    </IconButton>
             </div>
         </div>
     )

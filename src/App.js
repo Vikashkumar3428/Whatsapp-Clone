1b8f1994e98d911c1ac374b9fcc21ba0ca477e8a
Vikash Kumar <42893025+Vikashkumar3428@users.noreply.github.com>
2021-06-19 20:20:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
+import React from 'react';
 import Sidebar from './Sidebar';
 import Chat from './Chat';
 import { Route } from 'react-router-dom';
 import {BrowserRouter as Router, Switch} from 'react-router-dom';
+import Login from './Login';
+import {useStateValue} from './StateProvider';
 
 function App() {
-  return (
+  const [{ user }, dispatch] = useStateValue();
+  return (  
     // BEM  naming convention
     <div className="App">
+      {!user ? (
+        <Login />
+      ): (
         <div className="app__body">
           <Router>
             <Sidebar/> 
             <Switch>
-             
               <Route path ="/rooms/:roomId">
                   <Chat />
               </Route>
@@ -22,6 +28,8 @@ function App() {
             </Switch>
           </Router>
         </div>
+
+        )}
     </div>
   );
 }
